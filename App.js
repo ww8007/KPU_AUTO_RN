f70f2d35b299c78b213bdd16e784824c02e9074f
ww8007 <wshmin@naver.com>
2021-05-23 18:35:46 +0900

    Add Alert

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import React from "react";
 import {
-   StyleSheet,
-   Button,
-   View,
-   SafeAreaView,
-   Text,
-   Alert,
-   Image,
-   TextInput,
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+  Image,
+  TextInput,
 } from "react-native";
-import axios from "axios";
-import { oneDay, getDummy } from "./lib/api/post";
-import LoadingButton from "./Loading";
-import AnimateLoadingButton from "react-native-animate-loading-button";
-import ButtonSpinner from "react-native-button-spinner";
-import Icon from "react-native-vector-icons/AntDesign";
+
 import Icons from "react-native-vector-icons/MaterialCommunityIcons";
 import Apple from "react-native-vector-icons/AntDesign";
 import Buttons from "./components/view/Buttons";
@@ -26,155 +21,165 @@ import image2 from "./components/view/hi.png";
 const Separator = () => <View style={styles.separator} />;
 
 export default function App() {
-   const [id, onChangeText] = React.useState("");
-   const [pw, onChangePw] = React.useState("");
-   const [users, setUsers] = React.useState([
-      {
-         id: 1,
-         duration: "1일 외박 신청",
-         icon: "carrot",
-         color: "orange",
-      },
-      {
-         id: 2,
-         duration: "1주 외박 신청",
-         icon: "seedling",
-         color: "green",
-      },
-      {
-         id: 3,
-         duration: "2주 외박 신청",
-         icon: "dog",
-         color: "brown",
-      },
-      {
-         id: 4,
-         duration: "한 달 외박 신청",
-         icon: "crow",
-         color: "green",
-      },
-   ]);
-   const [mode, onChangeMode] = React.useState("1");
+  const [id, onChangeText] = React.useState("");
+  const [pw, onChangePw] = React.useState("");
+  const [users, setUsers] = React.useState([
+    {
+      id: 1,
+      duration: "1일 외박 신청",
+      icon: "carrot",
+      color: "#E56D29",
+    },
+    {
+      id: 2,
+      duration: "1주 외박 신청",
+      icon: "seedling",
+      color: "green",
+    },
+    {
+      id: 3,
+      duration: "2주 외박 신청",
+      icon: "dog",
+      color: "#8B5927",
+    },
+    {
+      id: 4,
+      duration: "한 달 외박 신청",
+      icon: "crow",
+      color: "green",
+    },
+  ]);
 
-   const awaitSendRequest = () => {
-      return new Promise((resolve, reject) => {
-         // oneDay({ id, pw, mode }).then(function (response) {
-         //   const { message } = JSON.parse(response.data.body);
-         //   onChangeResult(message);
-         //   resolve(message);
-         // });
-         onChangeResult("외박신청 완료");
-         resolve("hi");
-      });
-   };
+  const [mode, onChangeMode] = React.useState("1");
+  React.useEffect(() => {
+    Alert.alert(
+      "-주의사항-",
+      "서버오류가 나올 시 KPU 홈페이지가 잘 동작 하는지 확인해주세요.",
 
-   return (
-      <LinearGradient
-         colors={["#192f6a", "#3b5998", "#4c669f"]}
-         style={styles.container}
-      >
-         <SafeAreaView style={styles.items}>
-            <View style={styles.blankView}></View>
-            <Icons name="power-sleep" size={80} style={styles.title} />
-            <View style={{ flex: 2 }}></View>
-            <View style={styles.inputWithString}>
-               <TextInput
-                  style={styles.inputString}
-                  defaultValue="I D  : "
-               ></TextInput>
-               <TextInput
-                  style={styles.inputIdPw}
-                  autoCompleteType="username"
-                  value={id}
-                  autoCapitalize="none"
-                  onChangeText={(id) => onChangeText(id)}
-               />
-            </View>
-            <View style={styles.inputWithString}>
-               <TextInput
-                  style={styles.inputString}
-                  defaultValue="PW : "
-                  placeholderTextColor="white"
-               ></TextInput>
-               <TextInput
-                  style={styles.inputIdPw}
-                  autoCompleteType="password"
-                  secureTextEntry={true}
-                  onChangeText={(pw) => onChangePw(pw)}
-                  value={pw}
-               />
-            </View>
-            <View style={styles.blankView}></View>
+      [{ text: "확인", onPress: () => console.log("ok") }],
+      { cancelable: false }
+    );
+  }, []);
 
-            {users.map((user) => (
-               <Buttons user={user} key={user.id}></Buttons>
-            ))}
-            <View style={styles.blankView}></View>
-            <Image style={styles.stretch} source={image} />
-            <Image style={styles.stretch2} source={image2} />
-         </SafeAreaView>
-      </LinearGradient>
-   );
+  const awaitSendRequest = () => {
+    return new Promise((resolve, reject) => {
+      // oneDay({ id, pw, mode }).then(function (response) {
+      //   const { message } = JSON.parse(response.data.body);
+      //   onChangeResult(message);
+      //   resolve(message);
+      // });
+      onChangeResult("외박신청 완료");
+      resolve("hi");
+    });
+  };
+
+  return (
+    <LinearGradient
+      colors={["#192f6a", "#3b5998", "#4c669f"]}
+      style={styles.container}
+    >
+      <SafeAreaView style={styles.items}>
+        <View style={styles.blankView}></View>
+        <Icons name="power-sleep" size={80} style={styles.title} />
+        <View style={{ flex: 2 }}></View>
+        <View style={styles.inputWithString}>
+          <TextInput
+            style={styles.inputString}
+            defaultValue="I D  : "
+          ></TextInput>
+          <TextInput
+            style={styles.inputIdPw}
+            autoCompleteType="username"
+            value={id}
+            autoCapitalize="none"
+            onChangeText={(id) => onChangeText(id)}
+          />
+        </View>
+        <View style={styles.inputWithString}>
+          <TextInput
+            style={styles.inputString}
+            defaultValue="PW : "
+            placeholderTextColor="white"
+          ></TextInput>
+          <TextInput
+            style={styles.inputIdPw}
+            autoCompleteType="password"
+            secureTextEntry={true}
+            onChangeText={(pw) => onChangePw(pw)}
+            value={pw}
+          />
+        </View>
+        <View style={styles.blankView}></View>
+
+        {users.map((user) => (
+          <Buttons user={user} key={user.id}></Buttons>
+        ))}
+        <View style={styles.blankView}></View>
+        <Image style={styles.stretch} source={image} />
+        <Image style={styles.stretch2} source={image2} />
+      </SafeAreaView>
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-   },
-   items: {
-      flex: 1,
-      marginHorizontal: 12,
-   },
-   title: {
-      textAlign: "center",
-      marginVertical: 8,
-      color: "#FFD228",
-   },
-   fixToText: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-   },
-   separator: {
-      marginVertical: 8,
-      borderBottomColor: "#737373",
-      borderBottomWidth: StyleSheet.hairlineWidth,
-   },
-   inputIdPw: {
-      height: 50,
-      color: "white",
-      borderColor: "gray",
-      borderWidth: 1,
-      borderRadius: 11,
-      paddingLeft: 10,
-      flex: 8,
-      marginBottom: 10,
-   },
-   stretch: {
-      alignSelf: "flex-end",
-      marginLeft: 30,
-      flexDirection: "row-reverse",
-      flex: 2,
-      width: 60,
-      height: 70,
-      resizeMode: "stretch",
-   },
-   stretch2: {
-      alignSelf: "center",
-      resizeMode: "stretch",
+  container: {
+    flex: 1,
+  },
+  items: {
+    flex: 1,
+    marginHorizontal: 12,
+  },
+  title: {
+    textAlign: "center",
+    marginVertical: 8,
+    color: "#FFD228",
+  },
+  fixToText: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: "#737373",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  inputIdPw: {
+    height: 50,
+    color: "white",
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 11,
+    paddingLeft: 10,
+    flex: 8,
+    marginBottom: 10,
+  },
+  stretch: {
+    alignSelf: "flex-end",
+    marginLeft: 30,
+    flexDirection: "row-reverse",
+    flex: 2,
+    width: 60,
+    height: 70,
+    resizeMode: "stretch",
+  },
+  stretch2: {
+    alignSelf: "center",
+    resizeMode: "stretch",
 
-      flex: 4,
-      width: "120%",
-   },
-   inputString: {
-      flex: 1,
-      color: "white",
-   },
-   blankView: {
-      flex: 3,
-      justifyContent: "flex-end",
-      alignItems: "flex-end",
-   },
-   inputWithString: {
-      flexDirection: "row",
-   },
+    flex: 4,
+    width: "120%",
+  },
+  inputString: {
+    flex: 1,
+    color: "white",
+  },
+  blankView: {
+    flex: 3,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+  inputWithString: {
+    flexDirection: "row",
+  },
 });
--- components/view/Buttons.js
+++ components/view/Buttons.js
@@ -1,73 +1,88 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Alert } from "react-native";
 import ButtonSpinner from "react-native-button-spinner";
 import Apple from "react-native-vector-icons/FontAwesome5";
 import Apple2 from "react-native-vector-icons/AntDesign";
 import { oneDay, getDummy, getDummy2 } from "../../lib/api/post";
 export default function Buttons({ user }) {
-   const [result, onChangeResult] = React.useState("");
-   const [error, onChangeError] = React.useState("");
-   const awaitSendRequest = () => {
-      return new Promise((resolve, reject) => {
-         getDummy2()
-            .then(function (response) {
-               onChangeResult("완료");
-               resolve(response);
-            })
-            .catch(function (error) {
-               onChangeError("실패");
-               resolve(error);
-            });
-      });
-   };
-   const onChangeRe = () => {
-      onChangeError("");
-   };
-   return (
-      <>
-         {result ? (
-            <ButtonSpinner textButton={"Text Button"}>
-               <Apple2
-                  name="checkcircleo"
-                  size={20}
-                  color="white"
-                  style={{ height: "120%", marginBottom: 2 }}
-               />
-               <Text>{"  "}</Text>
-               <Text style={{ color: "white" }}> 신청 완료</Text>
-            </ButtonSpinner>
-         ) : error ? (
-            <ButtonSpinner textButton={"Text Button"} onPress={onChangeRe}>
-               <Apple2
-                  name="closecircleo"
-                  size={20}
-                  color="white"
-                  style={{ height: "120%", marginBottom: 2 }}
-               />
-               <Text>{"  "}</Text>
-               <Text style={{ color: "white" }}>서버 오류</Text>
-            </ButtonSpinner>
-         ) : (
-            <ButtonSpinner onPress={awaitSendRequest}>
-               <Apple
-                  name={user.icon}
-                  size={20}
-                  color={user.color}
-                  style={{ height: "120%", marginBottom: 2 }}
-               />
-               <Text>{"  "}</Text>
-               <Text style={{ color: "white" }}> {user.duration}</Text>
-            </ButtonSpinner>
-         )}
-      </>
-   );
+  const [result, onChangeResult] = React.useState("");
+  const [error, onChangeError] = React.useState("");
+  const awaitSendRequest = () => {
+    return new Promise((resolve, reject) => {
+      getDummy2()
+        .then(function (response) {
+          onChangeResult("완료");
+          resolve(response);
+        })
+        .catch(function (error) {
+          onChangeError("실패");
+          resolve(error);
+        });
+    });
+  };
+  const onChangeRe = () => {
+    onChangeError("");
+  };
+  const createTwoButtonAlert = () =>
+    Alert.alert(
+      user.duration,
+      "신청하시겠습니까?",
+
+      [
+        {
+          text: "취소",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        { text: "확인", onPress: () => awaitSendRequest() },
+      ],
+      { cancelable: false }
+    );
+  return (
+    <>
+      {result ? (
+        <ButtonSpinner textButton={"Text Button"}>
+          <Apple2
+            name="checkcircleo"
+            size={20}
+            color="white"
+            style={{ height: "120%", marginBottom: 2 }}
+          />
+          <Text>{"  "}</Text>
+          <Text style={{ color: "white" }}> 신청 완료</Text>
+        </ButtonSpinner>
+      ) : error ? (
+        <ButtonSpinner textButton={"Text Button"} onPress={onChangeRe}>
+          <Apple2
+            name="closecircleo"
+            size={20}
+            color="white"
+            style={{ height: "120%", marginBottom: 2 }}
+          />
+          <Text>{"  "}</Text>
+          <Text style={{ color: "white" }}>서버 오류</Text>
+        </ButtonSpinner>
+      ) : (
+        <ButtonSpinner onPress={createTwoButtonAlert}>
+          <Apple
+            name={user.icon}
+            size={20}
+            color={user.color}
+            style={{ height: "120%", marginBottom: 2 }}
+          />
+          <Text>{"  "}</Text>
+          <Text style={{ color: "white" }}> {user.duration}</Text>
+        </ButtonSpinner>
+      )}
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-   },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
